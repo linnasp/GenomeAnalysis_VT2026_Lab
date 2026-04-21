31195571602979f9e8dd117cd7b7546e755880b4
Linn Asp <linnasp@pelle1.uppmax.uu.se>
2026-04-21 20:57:17 +0200

    Finished FastQC for RNA slurm

---
+++ code/01_preprocessing/01_FastQC/20250421_FastQC_RNA.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 2
+#SBATCH -t 00:10:00
+#SBATCH -J FastQC_RNA
+#SBATCH --mail-type=ALL
+#SBATCH --output=/home/linnasp/GenomeAnalysis_VT2026_Lab/results/01_preprocessing/04_RNA/01_FastQC_RNA/outputfiles/%x.%j.out
+#SBATCH --error=/home/linnasp/GenomeAnalysis_VT2026_Lab/results/01_preprocessing/04_RNA/01_FastQC_RNA/errorfiles/%x.%j.err
+
+# Load modules
+
+module load FastQC/0.12.1-Java-17
+
+# Output
+out_dir=/home/linnasp/GenomeAnalysis_VT2026_Lab/results/01_preprocessing/04_RNA/01_FastQC_RNA
+
+# Your commands
+
+fastqc -t 2 \
+-o $out_dir \
+ /crex/proj/uppmax2026-1-61/Genome_Analysis/2_Zhou_2023/reads/transcriptomic_data/*.fq.gz
