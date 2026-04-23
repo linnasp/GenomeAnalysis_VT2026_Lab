b1524c9e7838f64d613a0182bd8f1addcf7ef4c4
Linn Asp <linnasp@pelle2.uppmax.uu.se>
2026-04-23 21:39:30 +0200

    Test run for Trimmomatic RNA

--- code/01_preprocessing/02_Trimmomatic/20260421_trimmomatic_RNA.sh
+++ code/01_preprocessing/02_Trimmomatic/20260421_trimmomatic_RNA.sh
@@ -11,9 +11,20 @@
 # Load modules
 module load Trimmomatic/0.39-Java-17
 
-trimmomatic PE -threads 2 \
-/crex/proj/uppmax2026-1-61/Genome_Analysis/2_Zhou_2023/reads/transcriptomic_data/*.fq.gz \
+input_dir=/crex/proj/uppmax2026-1-61/Genome_Analysis/2_Zhou_2023/reads/transcriptomic_data/
+output_dir=/home/linnasp/GenomeAnalysis_VT2026_Lab/results/01_preprocessing/04_RNA/02_Trimmomatic_RNA/testrun
+nr=0
 
-ouput fastqc
+for R1 in ${input_dir}/*_f1.fq.gz; do
+    R2=${R1/_f1.fq.gz/_r2.fq.gz}
+    ((nr++))
+    trimmomatic PE -threads 2 \
+    $R1 \
+    $R2 \
+    $output_dir/output_forward_paired_$nr.fq.gz \
+    $output_dir/output_forward_unpaired_$nr.fq.gz \
+    $output_dir/output_reverse_paired_$nr.fq.gz \
+    $output_dir/output_reverse_unpaired_$nr.fq.gz \
+    ILLUMINACLIP:$TRIMMOMATIC_HOME/adapters/TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+done
 
-ILLUMINACLIP:$TRIMMOMATIC_HOME/adapters/TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36 \
