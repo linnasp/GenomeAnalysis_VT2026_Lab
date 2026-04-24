350a725135691de6797698252eb2b422cb772fe7
Linn Asp <linnasp@pelle1.uppmax.uu.se>
2026-04-24 09:59:50 +0200

    Individual trimmomatic files for RNA

---
+++ code/01_preprocessing/02_Trimmomatic/20260424_trimmomatic_Control2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 1
+#SBATCH -t 00:30:00
+#SBATCH -J trimmomatic_RNA_control_2
+#SBATCH --mail-type=ALL
+#SBATCH --output=/home/linnasp/GenomeAnalysis_VT2026_Lab/results/01_preprocessing/04_RNA/02_Trimmomatic_RNA/outputfiles/%x.%j.out
+#SBATCH --error=/home/linnasp/GenomeAnalysis_VT2026_Lab/results/01_preprocessing/04_RNA/02_Trimmomatic_RNA/errorfiles/%x.%j.err
+
+# Load modules
+module load Trimmomatic/0.39-Java-17
+
+output_dir=/home/linnasp/GenomeAnalysis_VT2026_Lab/results/01_preprocessing/04_RNA/02_Trimmomatic_RNA/Trimmed_seq
+
+trimmomatic PE -threads 2 \
+/crex/proj/uppmax2026-1-61/Genome_Analysis/2_Zhou_2023/reads/transcriptomic_data/Control_2_f1.fq.gz \
+/crex/proj/uppmax2026-1-61/Genome_Analysis/2_Zhou_2023/reads/transcriptomic_data/Control_2_r2.fq.gz \
+$output_dir/Control_2_f_paired.fq.gz \
+$output_dir/Control_2_f_unpaired.fq.gz \
+$output_dir/Control_2_r_paired.fq.gz \
+$output_dir/Control_2_r_unpaired.fq.gz \
+ILLUMINACLIP:$TRIMMOMATIC_HOME/adapters/TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36 \
