2a55672f50cfd19f1faf639ac9568bea6d022320
Linn Asp <linnasp@pelle1.uppmax.uu.se>
2026-04-28 16:05:49 +0200

    Added all parameters to braker

--- code/05_annotation/01_BRAKER3/20250421_braker.sh
+++ code/05_annotation/01_BRAKER3/20250421_braker.sh
@@ -3,7 +3,7 @@
 #SBATCH -p pelle
 #SBATCH -c 2
 #SBATCH -t 00:10:00
-#SBATCH -J braker
+#SBATCH -J test_braker
 #SBATCH --mem=32G
 #SBATCH --mail-type=ALL
 #SBATCH --output=/home/linnasp/GenomeAnalysis_VT2026_Lab/results/05_annotation/01_BRAKER3/outputfiles/%x.%j.out
@@ -11,17 +11,29 @@
 
 module load BRAKER/3.0.8-foss-2024a
 
+export AUGUSTUS_CONFIG_PATH=/home/linnasp/bin/augustus_config
+
 # Input 
-softmask= output of RepeatMasker
-RNA_Seq=BAM fil from STAR
+softmask=/home/linnasp/GenomeAnalysis_VT2026_Lab/results/04_masking/02_RepeatMasker/01_run/pilon.fasta.masked
+hisat_dir=/home/linnasp/GenomeAnalysis_VT2026_Lab/results/06_DEA/01_HISAT2/results
+RNA_Seq=$hisat_dir/Control_1_sorted.bam,$hisat_dir/Control_2_sorted.bam,$hisat_dir/Control_3_sorted.bam,$hisat_dir/Heat_1_sorted.bam,$hisat_dir/Heat_2_sorted.bam,$hisat_dir/Heat_3_sorted.bam
+BRAKER_SIF=/crex/proj/uppmax2026-1-61/Genome_Analysis/2_Zhou_2023/braker3.sif
 
 # Output
-out_dir=/home/linnasp/GenomeAnalysis_VT2026_Lab/results/05_annotation/01_BRAKER3
+out_dir=/home/linnasp/GenomeAnalysis_VT2026_Lab/results/05_annotation/01_BRAKER3/results
+temporary_out_dir=/home/linnasp/GenomeAnalysis_VT2026_Lab/results/05_annotation/01_BRAKER3/temporary_files
 
-braker.pl \
---threads=2 \
---genome $softmask \
---bam $RNA_Seq \
---busco_lineage embryophyta_odb10 \
---workingdir=$out_dir
+export TMPDIR=$temporary_out_dir
 
+singularity exec \
+    -B /home/linnasp:/home/linnasp \
+    -B /crex/proj/uppmax2026-1-61:/crex/proj/uppmax2026-1-61 \
+    -B $AUGUSTUS_CONFIG_PATH:/opt/Augustus/config \
+    $BRAKER_SIF braker.pl \
+        --threads=2 \
+        --genome=$softmask \
+        --bam $RNA_Seq \
+        --workingdir=$out_dir \
+        --species=linnasp_Njap \
+        --softmasking \
+        --prot_seq=/home/linnasp/GenomeAnalysis_VT2026_Lab/results/05_annotation/01_BRAKER3/protein_evidence/Viridiplantae_filtered.fa
